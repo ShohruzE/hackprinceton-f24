d30cf4996af004e5139f8688db3c1fab1e4ccb58
michaelwong3049 <michaelwong3049@gmail.com>
2024-11-09 11:36:28 -0500

    Create nurse and patient interfaces, create database connection function, create patient and nurse collection

---
+++ lib/db/schemas/Patient.ts
@@ -0,0 +1,38 @@
+interface BloodPressure {
+    systolic: number,
+    diastolic: number,
+    timestamp: Date;
+}
+
+interface Temperature {
+    value: number;
+    timestamp: Date;
+}
+
+interface OxygenSaturation {
+    value: number;
+    timestamp: Date;
+}
+
+interface ConversationSummary {
+    summary: string;
+    timestamp: Date
+}
+
+export interface Patient {
+    first_name: string;
+    last_name: string
+    age: number;
+    blood_type: string;
+    allergies: Array<string>;
+    blood_pressure: BloodPressure[];
+    heart_rate: number;
+    temperature: Temperature[]
+    oxygen_saturation: OxygenSaturation[]
+    room_bed_number: string;
+    preferences: Array<string>;
+    nurse_notes: string,
+    required_tasks: Array<string>;
+    conversation_summary: ConversationSummary[],
+    //recordings
+}
