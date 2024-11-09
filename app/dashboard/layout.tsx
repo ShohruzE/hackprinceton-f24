bd5211a392da6d1a65d6a09b4a865cdef197acf7
gtomchuk2005 <gtomchuk2005@gmail.com>
2024-11-09 14:23:41 -0500

    Remove Dashboard header from layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/AppSidebar";
-import DashboardHeader from "@/components/DashboardHeader";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -8,7 +7,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <AppSidebar />
       <main className="flex h-screen w-full">
         <div className="grow bg-blue-50">
-          <DashboardHeader />
           <div className="p-4">{children}</div>
         </div>
       </main>
